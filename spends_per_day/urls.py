80fd3d5dd761ccadf14314fa1e9ad572fcc52c8a
Reynan Vieira <reynanvt@gmail.com>
2024-07-07 21:49:18 -0300

    Register and Authentication

--- spends_per_day/urls.py
+++ spends_per_day/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('google_auth/', include(('google_auth.urls', 'google_auth'),
+                                 namespace="google_auth")),
+    path("user/", include('user.urls')),
+
 ]
